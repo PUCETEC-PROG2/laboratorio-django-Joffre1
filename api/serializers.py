0b5fd136dd83346ae2c6c7fc2c940802b80b3d27
Joffre Verdezoto <joffre.verdezoto@uisek.edu.ec>
2026-01-06 12:34:27 -0500

    Entrenadores

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from pokedex.models import Pokemon
+from pokedex.models import Pokemon, Trainer
 from django.core.files.base import ContentFile
 import base64
 
@@ -22,4 +22,26 @@ class PokemonSerializer(serializers.ModelSerializer):
                 )
             except Exception:
                 raise serializers.ValidationError("La imagen no se encuentra con base64 válida.")
+        return value
+    
+class TrainerSerializer(serializers.ModelSerializer):
+    
+    pictureTrainer = serializers.CharField(required=False, allow_blank=True)
+
+    class Meta:
+        model = Trainer
+        fields = '__all__'
+
+    def validate_pictureTrainer(self, value):
+        if value:
+            try:
+                # Separamos el encabezado del contenido base64
+                format, imgstr = value.split(';base64,')
+                ext = format.split('/')[-1]
+                return ContentFile(
+                    base64.b64decode(imgstr),
+                    name=f'trainer_{ext}' # Nombre temporal del archivo
+                )
+            except Exception:
+                raise serializers.ValidationError("La imagen no es una cadena Base64 válida.")
         return value
